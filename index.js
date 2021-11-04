dc02385df0ac34de5dbfd530d779204fe63919b7
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-04 00:25:26 -0400

    commit tree bd786fe22b44840130e7c03272ff9b0cdf3e61ca

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`
+}   
+function mondayWork(task="go to the office"){
+    console.log(`This Monday, I will ${task}.`)
+    return `This Monday, I will ${task}.`
+}
+// function wrapAdjective(name="*"){
+//     const part1 = "You are"
+//         return function(){
+//             return `${part1} *${name}*!`;
+//     }
+// }
+    function wrapAdjective(flair="*"){
+        const innerFunction = function (adj = 'special'){
+            return `You are ${flair}${adj}${flair}!`;
+        }
+        return innerFunction 
+    }
+   
