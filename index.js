a27f965a37802f4e17641606165284b74eb5d574
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-04 00:36:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`
+}   
+function mondayWork(task="go to the office"){
+    console.log(`This Monday, I will ${task}.`)
+    return `This Monday, I will ${task}.`
+}
+    function wrapAdjective(flair="*"){
+        const innerFunction = function (adj = 'special'){
+            return `You are ${flair}${adj}${flair}!`;
+        }
+        return innerFunction 
+    }
+   
